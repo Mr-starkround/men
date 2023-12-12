e2e265f86be2b41286866e913882acbad820a570
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-12 19:03:47 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -46,7 +46,7 @@ async def send_menfess_handler(client: Client, msg: types.Message):
     db_user = db.get_data_pelanggan()
     db_bot = db.get_data_bot(client.id_bot).kirimchannel             
     keyboard = [
- [InlineKeyboardButton(                "👀ʟɪʜᴀᴛ", url="https://t.me/c/str(config.channel_1).split('-100')[1]/str{kirim.id}"),       InlineKeyboardButton(                "🗑ʜᴀᴘᴜs", callback_data="hps")],
+ [InlineKeyboardButton(                "👀ʟɪʜᴀᴛ", url="https://t.me/c/{link + str(kirim.id)}"),       InlineKeyboardButton(                "🗑ʜᴀᴘᴜs", callback_data="hps")],
 ]
     
     reply_markup = InlineKeyboardMarkup(keyboard)
