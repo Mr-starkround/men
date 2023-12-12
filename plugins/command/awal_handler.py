fae7bc233c9e0615e00962c6fef5be1364ab1582
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-13 03:56:38 +0700

    Update awal_handler.py

--- plugins/command/awal_handler.py
+++ plugins/command/awal_handler.py
@@ -255,7 +255,6 @@ async def cb_back(client: Client, query: CallbackQuery, msg: types.Message):
     helper = Helper(client, msg)
     user_id = callback_query.from_user.id
 first = msg.from_user.first_name
-    fullname = f'{first} {last}' if last else first
     username = (
         f'@{msg.from_user.username}'
         if msg.from_user.username
