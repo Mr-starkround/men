28129f1369472d0be666953a15d1c4909b522c81
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-12 14:39:25 +0700

    Update broadcast_handler.py

--- plugins/command/broadcast_handler.py
+++ plugins/command/broadcast_handler.py
@@ -38,7 +38,7 @@ async def broadcast_ya(client: Client, query: CallbackQuery):
     await msg.edit('Broadcast sedang berlangsung, tunggu sebentar', reply_markup = None)
     for user_id in user_ids:
         try:
-            await message.copy(user_ids)
+            await message.copy(user_id)
             berhasil += 1
         except FloodWait as e:
             await asyncio.sleep(e.x)
