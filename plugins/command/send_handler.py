6536b7ec81ee286cf13aa6ec018632487f891bd6
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-12-13 23:58:00 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -44,7 +44,6 @@ async def send_menfess_handler(client: Client, msg: types.Message, link: str = N
     keyboard = [
  [InlineKeyboardButton(                "👀ʟɪʜᴀᴛ", url=link + str(kirim.id)),       InlineKeyboardButton(                "🗑ʜᴀᴘᴜs", callback_data="hps")],
 ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
     if msg.text or msg.photo or msg.video or msg.voice:
         if msg.photo and not db_bot.photo:
             if db_user.status == 'member' or db_user.status == 'talent':
@@ -70,7 +69,7 @@ async def send_menfess_handler(client: Client, msg: types.Message, link: str = N
         kirim = await client.copy_message(config.channel_1, msg.from_user.id, msg.id, link=link + str(kirim.id))
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
         await db.update_menfess(coin, menfess, all_menfess)
-        await msg.reply(f"Pesan anda <a href='{link + str(kirim.id)}'>berhasil terkirim.</a> \n\nhari ini kamu telah mengirim pesan sebanyak {menfess + 1}/{config.batas_kirim}. kamu dapat mengirim pesan sebanyak {config.batas_kirim} kali dalam sehari. \n\nwaktu reset setiap jam 1 pagi", True, enums.ParseMode.HTML, reply_markup=reply_markup)
+        await msg.reply(f"Pesan anda <a href='{link + str(kirim.id)}'>berhasil terkirim.</a> \n\nhari ini kamu telah mengirim pesan sebanyak {menfess + 1}/{config.batas_kirim}. kamu dapat mengirim pesan sebanyak {config.batas_kirim} kali dalam sehari. \n\nwaktu reset setiap jam 1 pagi", quote=True, enums.ParseMode.HTML, reply_markup=InlineKeyboardMarkup(keyboard))
     else:
         await msg.reply('media yang didukung photo, video dan voice')
 
