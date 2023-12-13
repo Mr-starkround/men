63b2ac9b06efdcea95dd5ceaf420ca7611d25140
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-13 15:04:32 +0700

    Update helper.py

--- plugins/helper/helper.py
+++ plugins/helper/helper.py
@@ -27,7 +27,6 @@ class Helper():
         return text.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
 
     async def cek_langganan_channel(self, user_id: int):
-        
         if user_id == config.id_admin:
             return True
         try:
@@ -43,14 +42,14 @@ class Helper():
         ]
         return member.status in status
 
-    async def pesan_langganan(self):        
+    async def pesan_langganan(self):
         link_1 = await self.bot.export_chat_invite_link(config.channel_1)
         link_2 = await self.bot.export_chat_invite_link(config.channel_2)
-        markup = [
+        markup = InlineKeyboardMarkup([
             [InlineKeyboardButton('ɢʀᴏᴜᴘ ʙᴀsᴇ', url=link_2), InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ ʙᴀsᴇ', url=link_1)],
             [InlineKeyboardButton('ᴄᴏʙᴀ ʟᴀɢɪ', url='https://t.me/Jawafessbot?start=start')]
-        ]
-                 await self.bot.send_message(self.user_id, config.pesan_join, reply_to_message_id=self.message.id, reply_markup=markup)
+        ])
+        await self.bot.send_message(self.user_id, config.pesan_join, reply_to_message_id=self.message.id, reply_markup=markup)
 
     async def daftar_pelanggan(self):
         database = Database(self.user_id)
