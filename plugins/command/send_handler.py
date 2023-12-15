f89493c7756ddc6c8e1175fd8b577d79af0f6cd8
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-16 01:09:01 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -144,3 +144,13 @@ async def transfer_coin_handler(client: Client, msg: types.Message):
                 )
         else:
             return await msg.reply(f'<i>coin kamu ({my_coin}) tidak dapat transfer coin.</i>', True)
+
+async def hapus_pesan(client: Client, query: CallbackQuery):
+    try:
+        await query.message.reply_to_message.delete()
+    except:
+        pass
+    try:
+        await query.message.delete(link - str(kirim.id))
+    except:
+        pass
