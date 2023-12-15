b4521c54ea1d4351220bdf573c73c777aeb45de9
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-16 01:12:51 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -75,7 +75,7 @@ async def send_menfess_handler(client: Client, msg: types.Message, link: str = N
                 ),
                 InlineKeyboardButton(
                     "🗑ʜᴀᴘᴜs",
-                    callback_data"pus")
+                    callback_data="pus")
             ],
         ]
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))        
