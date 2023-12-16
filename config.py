6c7dc235479d375e2ba3bf9bcdb81c7dbde0583d
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-16 22:01:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ gagalkirim_msg = os.environ.get("GAGAL_KIRIM", """
 # =========================================================== #
 
 topup_msg = os.environ.get("PESAN_TOPUP", """
-Jawafess coin di gunakan untuk biaya mengirim menfess/promote ke @JAWAFES jika {batas_kirim}x batas kirim harian sudah habis. biaya untuk sekali mengirim adalah 25 coin.
+Jawafess coin di gunakan untuk biaya mengirim menfess/promote ke @JAWAFES jika 3x batas kirim harian sudah habis. biaya untuk sekali mengirim adalah 25 coin.
 
 ❏ Cara Membeli Coin Jawafess
 ├1. klik button top up dibawah ini
