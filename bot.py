5bbc78e25194430a6b8743db0f1de9cb95a35afb
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-18 05:23:31 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,3 +77,10 @@ class Bot(Client):
         a = requests.get(f'{url}/sendMessage?chat_id={config.channel_log}&text={pesan}&parse_mode=HTML').json()
         requests.post(f'{url}/pinChatMessage?chat_id={config.channel_log}&message_id={a["result"]["message_id"]}&parse_mode=HTML')
         requests.post(f'{url}/deleteMessage?chat_id={config.channel_log}&message_id={a["result"]["message_id"] + 1}&parse_mode=HTML')
+
+    async def hapus_pesan(self, x: str):
+        db = Database.get_pelanggan()        
+        url = f'https://api.telegram.org/bot{config.bot_token}'
+        a = requests.get(f'{url}/sendMessage?chat_id={config.channel_1}&text={pesan}&parse_mode=HTML').json()
+        requests.post(f'{url}/pinChatMessage?chat_id={config.channel_log}&message_id={a["result"]["message_id"]}&parse_mode=HTML')
+        requests.post(f'{url}/deleteMessage?chat_id={config.channel_log}&message_id={a["result"]["message_id"] + 1}&parse_mode=HTML')
