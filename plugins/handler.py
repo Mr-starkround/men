70aa92aca726d2f91228fcb09754df69e1817836
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-23 23:28:38 +0700

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -133,8 +133,8 @@ async def on_message(client: Client, msg: Message):
             else:
                 await gagal_kirim_handler(client, msg)
 
-                      elif key in username:                   
-         if msg.from_user.username is None:
+                   elif key in username:                   
+       if msg.from_user.username is None:
                        return await msg.reply('Anda harus memiliki username untuk mengirim menfess.', True, enums.ParseMode.HTML)
                     else:
                         return await send_menfess_handler(client, msg)
