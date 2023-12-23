5e2fc1cebcc0c186f2db87aae01f2e3e7b2248b8
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-24 00:55:03 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -60,14 +60,6 @@ async def send_menfess_handler(client: Client, msg: types.Message, link: str = N
     db = Database(msg.from_user.id)
     db_user = db.get_data_pelanggan()
     db_bot = db.get_data_bot(client.id_bot).kirimchannel
-buttons = [
-      [
-  InlineKeyboardButton(
-                "Rules", url="https://t.me/JAWAFES/28266"
-            ),
-    ],
-]
-
 
     if msg.text or msg.photo or msg.video or msg.voice:
         if msg.photo and not db_bot.photo:
@@ -106,6 +98,15 @@ buttons = [
  ),
 
         # Use regular expression to check for links in the message
+
+buttons = [
+      [
+  InlineKeyboardButton(
+                "Rules", url="https://t.me/JAWAFES/28266"
+            ),
+    ],
+]
+
         if re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", msg.text or ""):
             return await msg.reply("Tidak diizinkan mengirimkan tautan.",        disable_web_page_preview=True,        reply_markup=InlineKeyboardMarkup(buttons),
         quote=True
