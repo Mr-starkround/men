5294e65e431f2d9e0ae25696a3c4116e93e27075
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-24 19:09:17 +0700

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -181,7 +181,7 @@ keyboard = [
                 await gagal_kirim_handler(client, msg)
    
         
-       elif msg.chat.type == enums.ChatType.SUPERGROUP:
+      if msg.chat.type == enums.ChatType.SUPERGROUP:
         command = msg.text or msg.caption
         if msg.from_user is None:
             if msg.sender_chat.id != config.channel_1:
