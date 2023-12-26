1ea97a567d512d35c757e1cee1345c27df244323
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-26 13:06:48 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -62,6 +62,12 @@ async def send_menfess_handler(client: Client, msg: types.Message, link: str = N
     db = Database(msg.from_user.id)
     db_user = db.get_data_pelanggan()
     db_bot = db.get_data_bot(client.id_bot).kirimchannel
+
+    keyboard = [
+        [InlineKeyboardButton(                "ᴄᴀʀᴀ ᴛᴏᴘ ᴜᴘ ᴄᴏɪɴ ᴊᴀᴡᴀꜰᴇꜱꜱ", callback_data="tpp")],  
+ ]   
+reply_markup = InlineKeyboardMarkup(keyboard)        
+
     if msg.text or msg.photo or msg.video or msg.voice:
         if msg.photo and not db_bot.photo:
             if db_user.status == 'member' or db_user.status == 'talent':
@@ -96,12 +102,6 @@ async def send_menfess_handler(client: Client, msg: types.Message, link: str = N
                     mentioned_username = msg.text[entity.offset:entity.offset + entity.length].lower()
                     # If the mentioned username is not the sender's username, reject the message
                     if mentioned_username != username:
-  
-keyboard = [
-        [InlineKeyboardButton(                "ᴄᴀʀᴀ ᴛᴏᴘ ᴜᴘ ᴄᴏɪɴ ᴊᴀᴡᴀꜰᴇꜱꜱ", callback_data="tpp")],  
- ]   
-reply_markup = InlineKeyboardMarkup(keyboard)        
-
 
                         return await msg.reply(f"Anda hanya dapat mengirim menfess dengan menggunakan username Anda sendiri.",        disable_web_page_preview=True,        reply_markup=markup,
         quote=True
