c292662465d07d7db637c000a3ab206accc10447
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-26 21:03:14 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -55,6 +55,9 @@ async def send_menfess_handler(client: Client, msg: types.Message, link: str = N
     db = Database(msg.from_user.id)
     db_user = db.get_data_pelanggan()
     db_bot = db.get_data_bot(client.id_bot).kirimchannel
+
+
+
  buttons = [
             [
                 InlineKeyboardButton(
