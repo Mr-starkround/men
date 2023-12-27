bd0de03094c0249a638a0892f48a9b7cc731a9f1
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-27 13:25:45 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -1,3 +1,4 @@
+
 import config
 import re
 from pyrogram import Client, types, enums
@@ -7,27 +8,6 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
     db = Database(msg.from_user.id)
     helper = Helper(client, msg)
     user = db.get_data_pelanggan()
-
-    # Check if the sender has a username
-    if msg.from_user.username is None:
-        return await msg.reply('Anda harus memiliki username untuk mengirim menfess.', quote=True)
-
-    # Check if the message mentions the sender's username
-    username = f"@{msg.from_user.username}".lower() if msg.from_user.username else None
-    if username and username not in msg.text.lower():
-        return await msg.reply('Anda hanya dapat mengirim menfess dengan menggunakan username Anda sendiri.', quote=True)
-
-    # Check if the user is authorized to send messages
-    if user.status not in ['owner', 'admin', 'talent', 'daddy sugar']:
-        # Check if the message mentions usernames from the admin list
-        admin_usernames = ["@ownneko", "@satt329", "@nekojoyy", "@winnieewwe", "@mwehehe0j", "@ikeenandrasw", "@sasaanmf", "@lordmudaid", "@towirg", "@suunshiinneee", "@kjitten"]
-        for admin_username in admin_usernames:
-            if admin_username in msg.text.lower():
-                return await msg.reply(f'Maaf, Anda tidak diizinkan mengirim pesan yang mengandung username member premium {admin_username}.', quote=True)
-
-    # Check for URLs in the message
-    if re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", msg.text or ""):
-        return await msg.reply("Tidak diizinkan mengirimkan tautan.")
 
     if msg.text or msg.photo or msg.video or msg.voice:
         menfess = user.menfess
@@ -64,8 +44,21 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
         caption = msg.text or msg.caption
         entities = msg.entities or msg.caption_entities
 
-        # Remove URLs from caption
-        caption = re.sub(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "", caption)
+        # Check if the message mentions the sender's username
+        username = f"@{msg.from_user.username}".lower() if msg.from_user.username else None
+
+        # Check if the message contains mentions of other usernames
+        if msg.entities:
+            for entity in msg.entities:
+                if entity.type == "mention":
+                    mentioned_username = msg.text[entity.offset:entity.offset + entity.length].lower()
+                    # If the mentioned username is not the sender's username, reject the message
+                    if mentioned_username != username:
+                        return await msg.reply('Anda hanya dapat mengirim menfess dengan menggunakan username Anda sendiri.', quote=True)
+
+        # Use regular expression to check for links in the message
+        if re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", msg.text or ""):
+            return await msg.reply("Tidak diizinkan mengirimkan tautan.", quote=True)
 
         kirim = await client.send_photo(config.channel_1, picture, caption, caption_entities=entities)
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
