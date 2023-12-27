83015820c18803da1df2ec1cb3f8d6de170d447b
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-27 17:25:24 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -64,7 +64,14 @@ async def send_menfess_handler(client: Client, msg: types.Message, link: str = N
                     return await msg.reply(f'Pesanmu gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali. Coin mu kurang untuk mengirim menfess diluar batas harian. \n\nwaktu reset jam 1 pagi \n\nKamu dapat mengirim menfess kembali pada esok hari atau top up coin untuk mengirim diluar batas harianmu. \n\n<b>Topup Coin silahkan klik</b> /topup', True, enums.ParseMode.HTML)
 
         link = await get_link()
-  
+
+
+      keyboard = [
+        [InlineKeyboardButton(                "ᴄᴀʀᴀ ᴛᴏᴘ ᴜᴘ ᴄᴏɪɴ ᴊᴀᴡᴀꜰᴇꜱꜱ", callback_data="tpp")],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)        
+
+
     # Check if the sender has a username
     if msg.from_user.username is None:
         return await msg.reply('Anda harus memiliki username untuk mengirim menfess.', quote=True)
