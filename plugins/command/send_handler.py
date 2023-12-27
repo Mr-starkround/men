e6662934482c55d85879fe60e35785afc9328056
Noob <78058269+Mr-starkround@users.noreply.github.com>
2023-12-27 18:53:49 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -5,7 +5,7 @@ from pyrogram import Client, types, enums
 from plugins import Database, Helper
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-async def send_with_pic_handler(self, client: Client, msg: types.Message, key: str, hastag: list):
+async def send_with_pic_handler(client: Client, msg: types.Message, key: str, hastag: list):
     db = Database(msg.from_user.id)
     helper = Helper(client, msg)
     user = db.get_data_pelanggan()   
@@ -51,7 +51,7 @@ async def send_with_pic_handler(self, client: Client, msg: types.Message, key: s
     else:
         await msg.reply('media yang didukung photo, video dan voice')
 
-async def send_menfess_handler(self, client: Client, msg: types.Message, link: str = None):
+async def send_menfess_handler(client: Client, msg: types.Message, link: str = None):
     helper = Helper(client, msg)
     db = Database(msg.from_user.id)
     db_user = db.get_data_pelanggan()
